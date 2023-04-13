be17bd40c83f6ea6e23d5cb162ce529803bbac88
reedjohnkenny <72041006+reedjohnkenny@users.noreply.github.com>
2023-04-13 13:38:29 -0700

    Fix colors

--- Lewisia_analyses.R
+++ Lewisia_analyses.R
@@ -24,7 +24,10 @@ pcr_inat_dat <- prcomp(Inat_stats)
 
 autoplot(pcr_inat_dat, data = Inat_data, colour = 'Taxon', loadings = TRUE, loadings.label = TRUE, loadings.colour = "black", loadings.label.size = 4, loadings.label.colour = "black") +
   geom_point(aes(fill = Taxon, shape = Taxon), colour = "black", size = 2) +
-  scale_fill_manual(values = c("L. nevadensis" = "honeydew", "San Juan Ridge population" = "violet")) +
+  stat_ellipse(aes(color = Taxon)) +
+  stat_ellipse(geom = "polygon", aes(fill = Taxon), alpha = 0.1) +
+  scale_fill_manual(values = c("L. nevadensis" = "#a6a6a6", "San Juan Ridge population" = "#a962f7")) +
+  scale_color_manual(values = c("L. nevadensis" = "#a6a6a6", "San Juan Ridge population" = "#a962f7")) +
   scale_shape_manual(values = c(21, 24)) +
   theme_bw() + 
   theme(panel.grid = element_blank()) +
@@ -58,26 +61,34 @@ Lewisia_morpho_comp <- Lewisia_morpho_comp %>% mutate(leaf_ln_wd_ratio = basal_l
 
 pcr_morpho_dat <- prcomp(Lewisia_morpho_comp)
 
-autoplot(pcr_morpho_dat, data = Lewisia_morpho, colour = 'Taxon', loadings = TRUE, frame = TRUE, frame.type = 'norm', loadings.label = TRUE, loadings.colour = "black", loadings.label.size = 4, loadings.label.colour = "black") +
+autoplot(pcr_morpho_dat, data = Lewisia_morpho, colour = 'Taxon', loadings = TRUE, frame = FALSE, frame.type = 'norm', frame.colour = "Taxon", loadings.label = TRUE, loadings.colour = "black", loadings.label.size = 4, loadings.label.colour = "black") +
   geom_point(aes(fill = Taxon, shape = Taxon), colour = "black", size = 2) +
   scale_fill_manual(values = c("L. nevadensis" = "#a6a6a6", "San Juan Ridge population" = "#a962f7")) +
+  stat_ellipse(aes(color = Taxon)) +
+  stat_ellipse(geom = "polygon", aes(fill = Taxon), alpha = 0.1) +
+  scale_color_manual(values = c("L. nevadensis" = "#a6a6a6", "San Juan Ridge population" = "#a962f7")) +
   scale_shape_manual(values = c(21, 24)) +
-  theme_bw() + 
+  theme_bw() +
   theme(panel.grid = element_blank()) +
-  labs(title = "PCA of Lewisia Morphological Traits") + 
+  labs(title = "PCA of Lewisia Morphological Traits") +
   theme(text = element_text(size = 16, family = "Times New Roman")) +
-  theme(plot.title = element_text(hjust = 0.5, size = 20)) 
+  theme(plot.title = element_text(hjust = 0.5, size = 20))
 ggsave("PCAofLewisiaMorphoTraits.png", dpi=300, height=6, width=9, units="in")
 
-# Original colors 
-# autoplot(pcr_morpho_dat, data = Lewisia_morpho, colour = 'Taxon', loadings = TRUE, frame = TRUE, frame.type = 'norm', loadings.label = TRUE, loadings.colour = "black", loadings.label.size = 4, loadings.label.colour = "black") +
-#   geom_point(aes(fill = Taxon, shape = Taxon), colour = "black", size = 2) +
-#   scale_fill_manual(values = c("L. nevadensis" = "honeydew", "San Juan Ridge population" = "violet")) +
-#   scale_shape_manual(values = c(21, 24)) +
-#   theme_bw() + 
-#   theme(panel.grid = element_blank()) +
-#   labs(title = "PCA of Lewisia Morphological Traits") + 
-#   theme(text = element_text(size = 16, family = "Times New Roman")) +
-#   theme(plot.title = element_text(hjust = 0.5, size = 20)) 
-# ggsave("PCAofLewisiaMorphoTraits.png", dpi=300, height=6, width=9, units="in")
+ggplot(pcr_morpho_dat, aes(fill = Taxon, shape = Taxon, colour = "black"))
+
+#Original colors
+autoplot(pcr_morpho_dat, data = Lewisia_morpho, colour = 'Taxon', loadings = TRUE, frame = FALSE, frame.type = 'norm', frame.colour = "Taxon", loadings.label = TRUE, loadings.colour = "black", loadings.label.size = 4, loadings.label.colour = "black") +
+  geom_point(aes(fill = Taxon, shape = Taxon), colour = "black", size = 2) +
+  scale_fill_manual(values = c("L. nevadensis" = "honeydew", "San Juan Ridge population" = "violet")) +
+  stat_ellipse(aes(color = Taxon)) +
+  stat_ellipse(geom = "polygon", aes(fill = Taxon), alpha = 0.25) +
+  scale_color_manual(values = c("L. nevadensis" = "honeydew", "San Juan Ridge population" = "violet")) +
+  scale_shape_manual(values = c(21, 24)) +
+  theme_bw() +
+  theme(panel.grid = element_blank()) +
+  labs(title = "PCA of Lewisia Morphological Traits") +
+  theme(text = element_text(size = 16, family = "Times New Roman")) +
+  theme(plot.title = element_text(hjust = 0.5, size = 20))
+ggsave("PCAofLewisiaMorphoTraits.png", dpi=300, height=6, width=9, units="in")
 
